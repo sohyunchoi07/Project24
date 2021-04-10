f115ca961fa48f17bb004f5652c635c0429ec402
sohyunchoi07 <79302887+sohyunchoi07@users.noreply.github.com>
2021-04-10 15:23:27 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer,rubber,iron,stone;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber = new Rubber(1000,580,70);
+    iron = new Iron(800,500);
+    stone = new Stone(600,500,100,100);
+
+    
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    iron.display();
+    stone.display();
+}
